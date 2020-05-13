200d737e50da339c4e3e5d3c3d1a3483934eaeef
RutujaGandhi <65295738+RutujaGandhi@users.noreply.github.com>
2020-05-13 20:06:23 +0530

    myscript.js

--- myscript.js
+++ myscript.js
@@ -1,10 +1,10 @@
 var randomNo1 = Math.floor(Math.random()*6) + 1;
-var randomImageSource1= "images/"+"dice"+randomNo1+".png";
+var randomImageSource1= "dice"+randomNo1+".png";
 document.querySelectorAll("img")[0].setAttribute("src",randomImageSource1);
 
 
 var randomNo2 =Math.floor(Math.random()*6) + 1;
-var randomImageSource2= "images/"+"dice"+randomNo2+".png";
+var randomImageSource2= "dice"+randomNo2+".png";
 document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
 
 if(randomNo1>randomNo2)
